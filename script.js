862ba68310ccca731df8d1edbeb50ad38c308317
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-09-08 12:09:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 //your code here
 function handleClick() {
+var name = document.getElementById("name").value
+var price = document.getElementById("price").value
+var table = document.getElementsByTagName("")[0];
+
+var gt = document.getElementById("gt").value
+	
 	
 }
