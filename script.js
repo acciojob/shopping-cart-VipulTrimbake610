c69637c745f196fbadcc2444a3e53bf5281d6a16
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-09-10 22:13:38 +0530

    created a submission

--- script.js
+++
@@ -1,9 +0,0 @@
- //your code here
-        function handleClick() {
-            var name = document.getElementById("item-name-input").value
-            var price = document.getElementById("item-price-iput").value
-            var table = document.getElementsByTagName("table")[0];
-            // var gt = document.getElementById("").value
-
-
-        }
